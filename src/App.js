8a57c5948b1d5b7c473f0880b392e69d6129914a
Rita Daukšienė <rita78@gmail.com>
2023-01-14 20:25:59 +0200

    message new

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> ?????and save to reloadddddddddd.
+          Edit <code>src/App.js</code> ?????and save to reloadddddddddd AAAAAA.
         </p>
       </header>
     </div>
